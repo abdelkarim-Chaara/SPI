126d0ca31d9abf28531e2b2a4aff93a3521efa8d
KARIM <abdelkarim.chaara@gmail.com>
2019-03-14 14:08:02 +0100

    Controller CGREFCodes Work Well

--- src/main/java/fr/univbrest/dosi/business/CgRefCodeBusinessJPA.java
+++ src/main/java/fr/univbrest/dosi/business/CgRefCodeBusinessJPA.java
@@ -2,9 +2,11 @@ package fr.univbrest.dosi.business;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fr.univbrest.dosi.bean.CgRefCode;
+import fr.univbrest.dosi.repository.CandidatRepository;
 import fr.univbrest.dosi.repository.CgRefCodeRepository;
 
 @Service
@@ -12,6 +14,12 @@ import fr.univbrest.dosi.repository.CgRefCodeRepository;
 public class CgRefCodeBusinessJPA implements CgRefCodeBusiness {
 
 	CgRefCodeRepository repo;
+	
+	@Autowired
+	public CgRefCodeBusinessJPA(CgRefCodeRepository repos) {
+		this.repo = repos;
+	}
+	
 	@Override
 	public List<CgRefCode> GetAllCgRefCodes() {
 		// TODO Auto-generated method stub
